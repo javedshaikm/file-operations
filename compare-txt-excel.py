c273097e27df24a118286b3ebf28c5f4a92ae74d
javedshaikm <javed.s.m@hotmail.com>
2018-09-23 06:02:12 -0400

    Update compare-txt-excel.py

--- compare-txt-excel.py
+++ compare-txt-excel.py
@@ -1,16 +1,14 @@
 import xlrd
-#Location of the text file 
 loc = ('C:\\Users\\shaikjav\\Desktop\\Pending banner.xlsx')
-#location of the excel file to be compared
 txt = open('C:\\Users\\shaikjav\\Desktop\\Pending-banner.txt').read().splitlines()
 # To open Workbook 
 wb = xlrd.open_workbook(loc) 
 sheet = wb.sheet_by_index(0) 
-ddc = sheet.cell_value(0,0)
+#ddc = sheet.cell(0,0)
 for i in range(sheet.nrows): 
-   for lines in txt:
-    	if sheet.cell_value(i, 5) == lines:
-    		print (f"{sheet.cell_value(i, 5)} is present")
-    	else:
-    		pass
+	for lines in txt:
+		if  lines in sheet.cell_value(i, 5):
+			print (f"{sheet.cell_value(i, 1)} is equal to {lines}")
+		else:
+			pass
     		#print (f"{sheet.cell_value(i, 1)} is not equal to {lines}")
